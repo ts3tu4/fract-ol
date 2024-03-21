492ab6ef1caa085b788b69e069cf062527d95e5e
mnanke <c11421nm@gmail.com>
2024-03-21 18:11:34 +0900

    エラーのコード書けてないので大丈夫かわからんけどとりあえずOK　もしかしたら最後に0が付く処理いるかも

--- utill.c
+++ utill.c
@@ -6,7 +6,7 @@
 /*   By: mnanke <mnanke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 19:53:22 by mnanke            #+#    #+#             */
-/*   Updated: 2024/03/09 20:08:09 by mnanke           ###   ########.fr       */
+/*   Updated: 2024/03/21 18:11:04 by mnanke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,52 @@ int	ft_strcmp(const char *s1, const char *s2)
 		i++;
 	}
 	return (0);
+}
+
+double	ft_atof_process(char *s, double dot, int i, double rtn)
+{
+	while (s[i])
+	{
+		if (!ft_isdigit(s[i]))
+		{
+			if (s[i] == '.')
+				break ;
+			ft_error();
+		}
+		rtn = rtn * 10 + s[i] - '0';
+		i++;
+	}
+	if (s[i] && s[i] == '.')
+	{
+		i++;
+		while (s[i])
+		{
+			if (!ft_isdigit(s[i]))
+				ft_error();
+			rtn = rtn * 10 + s[i] - '0';
+			i++;
+			dot *= 10;
+		}
+	}
+	return (rtn / dot);
+}
+
+double	ft_atof(char *s)
+{
+	double	rtn;
+	double	symbol;
+	double	dot;
+	int		i;
+
+	symbol = 1;
+	dot = 1;
+	rtn = 0;
+	i = 0;
+	if (s[i] == '-')
+	{
+		symbol *= -1;
+		i++;
+	}
+	rtn = (ft_atof_process(s, dot, i, rtn) * symbol);
+	return (rtn);
 }
